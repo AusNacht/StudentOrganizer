c2ec7688eb2fc2ca13018ea01627677fac260115
Aaron Lathrop <alathrop@diversecomputing.com>
2019-06-15 12:33:55 -0400

    v ,

--- StudentOrganizer/Controllers/StudentController.cs
+++ StudentOrganizer/Controllers/StudentController.cs
@@ -4,18 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StudentOrganizer.Dao;
 using StudentOrganizer.Models;
 
 namespace StudentOrganizer.Controllers
 {
     public class StudentController : Controller
     {
+        UniversityDatabase _db = new UniversityDatabase();
+
         public IActionResult StudentView()
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
+            //To get stuff from the URL
+            //var controller = RouteData.Values["controller"];
+            //var action = RouteData.Values["action"];
+            //var id = RouteData.Values["id"];
 
+            var model = _db.Students.ToList();
+
+            //Add stuff to the model
             List<StudentModel> students = new List<StudentModel>();
             students.Add(
                 new StudentModel
@@ -27,12 +34,21 @@ namespace StudentOrganizer.Controllers
                     Status = "Active"
                 }
             );
-            var model = new StudentListModel
-            {
-                Students = students
-            };
+            //var model = new StudentListModel
+            //{
+            //    Students = students
+            //};
 
             return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ StudentOrganizer/Dao/UniversityDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+using StudentOrganizer.Entities;
+
+namespace StudentOrganizer.Dao
+{
+    public class UniversityDatabase : DbContext
+    {
+       public DbSet<Student> Students { get; set; }
+    }
+}
